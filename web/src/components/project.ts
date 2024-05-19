912c75c1bcab27762d3fef16ac7fb1f91ced8d7b
Sirbarry <lieven3.schokker@gmail.com>
2024-05-19 19:51:51 +0200

    some fixes

--- web/src/components/project.ts
+++ web/src/components/project.ts
@@ -35,72 +35,100 @@ export class Project extends LitElement {
             margin: 0;
         }
 
-        .filled {
-            z-index: 10;
+        section {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: flex-start;
+            gap: 0.25rem;
+            margin: 10rem 3rem 20rem 3rem;
+            width: 55rem;
+        }
+
+        .card {
+            display: flex;
             position: relative;
-            background-color: white;
-            bottom: 5em;
-            left: 5em;
+            background-color: rgb(243, 244, 246);
+            box-sizing: border-box;
+            margin-left: 2em;
+            border: 2px solid white;
+            border-radius: 0.5rem;
+            width: 50%;
+        }
+
+        .content {
+            padding: 1.25rem;
+            position: relative;
+            z-index: 10;
+            display: block;
+        }
+
+        span {
+            position: absolute;
+            border-radius: 0.5rem;
+            min-width: 100%;
+            min-height: 100%;
+            top: 4rem;
+            right: 2rem;
+            border: 2px solid white;
+            z-index: 0;
+        }
+
+        .small-content {
+            display: flex;
+            position: absolute;
+            left: 1rem;
+            bottom: 1.25rem;
+            justify-content: space-between;
+            align-items: center;
         }
 
         img {
-            width: 20em;
-            height: 20em;
-            border-radius: 5em;
-            padding: 2em;
-        }
-
-        .border {
-            position: absolute;
-            z-index: 0;
-            border-style: solid;
-            border-radius: .5rem;
-            border-color: rgb(255 255 255);
-            border-width: 2px;
-            padding: 4em;
-            bottom: 5em;
-        }
-
-        container {
-            display: flex;
-            flex-direction: row;
-        }
-
-        card {
-            display: flex;
-            position: relative;
-            margin-left: auto;
+            object-fit: contain;
+            border-radius: 0.5rem;
+            width: 100%;
+            height: 100%;
         }
 
         .link {
             text-decoration: underline;
-            text-decoration-thickness: 2px;
         }
 
-        div {
+        .textBox {
             display: flex;
-            margin-left: 3em;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: flex-start;
+            gap: 0.25rem;
+            margin: 3rem;
+            width: 50%;
         }
     `;
 
     protected render(): TemplateResult {
         return html`
-            <container>
-                <article>
-                    <h2>This is the title of my projec</h2>
-                    <h3>I learned a lot</h3>
-                    <p>wow</p>
-                </article>
-                <div class="card">
-                    <article class="filled">
-                        <img src="../../public/img/thumbnails/vrGame.png" alt="vrGame">
-                    </article>
-                    <article class="border">
-                        <h2>This project can be found on github:</h2>
-                        <h3 class="link">github</h3>
-                    </article>
+            <section>
+                <div class="textBox">
+                    <h2>Unity Engine</h2>
+                    <h1>Virtual Synthesizer</h1>
+                    <p>In this project, I was experimenting with <strong>Unity</strong> in VR. <strong>The
+                        goal</strong> of the project
+                        was to make sound
+                        design fun. By hitting the instrument in different places and with different velocities, you can
+                        create a variety of unique sounds.</p>
                 </div>
-            </container>
+                <div class="card">
+                    <div class="content">
+                        <img src="../../public/img/thumbnails/vrGame.png" alt="vrGame">
+                    </div>
+                    <span>
+                        <div class="small-content">
+                            <p>This project is available on: </p>
+                            <h3 href="https://gitlab.fdmci.hva.nl/schokkl1/unityvr" class="link">github</h3>
+                        </div>
+                    </span>
+                </div>
+            </section>
         `;
     }
 }
