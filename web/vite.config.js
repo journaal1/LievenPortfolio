298523e6d09734e9063ec68ae5f535dc778eeda2
Sirbarry <lieven3.schokker@gmail.com>
2025-12-02 20:44:16 +0100

    fix: vite js pathing

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   base: "/",
   build: {
     outDir: "dist",
   },
+  resolve: {
+    alias: {
+      "three": path.resolve(__dirname, "node_modules/three"),
+      "three/addons/": path.resolve(__dirname, "node_modules/three/examples/jsm/"),
+      "three.meshline": path.resolve(__dirname, "node_modules/three.meshline"),
+    }
+  }
 });
